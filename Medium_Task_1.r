0bb40332b8219074f6e103e85ed54cd080462b72
Sonali Sharma <49304397+sonali2105@users.noreply.github.com>
2020-03-16 00:58:23 +0530

    add the comments

--- Medium_Task_1.r
+++ Medium_Task_1.r
@@ -1,19 +1,35 @@
+#importing library for sqlite
 library(RSQLite)
+
+#setting up the connection
 connection <- dbConnect(SQLite(), "survey.db")
+
+#printing the lists of table in the database
 dbListTables(connection)
+
+#creating a new table
 dbWriteTable(connection, "table1", mtcars)
-dbListTables(connection)
+
+#showing the attributes of the table
 dbListFields(connection, "table1")
+
+#printing the whole table
 dbReadTable(connection,"table1")
-searchFunction <- function(query,weight,gear) {
+
+#it is our search function that takes input for the query 
+searchFunction <- function(weight,gear) {
+  #its our query which will display the data according to the inputs 
   
+  query <- "SELECT *  FROM table1 WHERE  gear <= ? AND mpg = ?"
   return(dbGetPreparedQuery(connection, query, data.frame(weight,gear)))
 }
-query <- "SELECT *  FROM table1 WHERE  gear <= ? AND mpg = ?"
+
 gear<- 4
 weight<-21.0
-a<-searchFunction(query,gear,weight)
+#function call
+a<-searchFunction(gear,weight)
 print(a)
 
+#closing the connection
 dbDisconnect(connection)
 
