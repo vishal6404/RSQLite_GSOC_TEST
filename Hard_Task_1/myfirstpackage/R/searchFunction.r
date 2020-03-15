20c9ab8b76ce5897211a58b2b4a24661906aa846
Sonali Sharma <49304397+sonali2105@users.noreply.github.com>
2020-03-16 01:01:34 +0530

    update searchFunction.r

--- Hard_Task_1/myfirstpackage/R/searchFunction.r
+++ Hard_Task_1/myfirstpackage/R/searchFunction.r
@@ -1,3 +1,6 @@
+#@export tag above your function to indicate this function to be “exposed” to users to use.
+#The #' @export syntax is actually an Roxygen tag.
+
 #' @export
 searchFunction <- function(weight,gear) {
   query <- "SELECT *  FROM table1 WHERE  gear <= ? AND mpg = ?"
